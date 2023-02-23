fb897a3dfcd0ad4b8eeeb6c4c436f772bab3d140
Tomasz Jagiełka <thomas.jagielka@gmail.com>
2023-02-23 02:19:55 +0100

    Add 121. Best Time to Buy and Sell Stock

---
+++ C-Sharp/C-Sharp/121BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    // TODO:
+    // Fix Time Limit Exceeded.
+
+    internal class _121BestTimeToBuyAndSellStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int[] point = new int[2];
+            int bestBuyPoint = -1;
+            int bestSellPoint = -1;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i; j < prices.Length; j++)
+                {
+                    if (bestBuyPoint == -1 || bestSellPoint == -1 ||
+                        prices[j] - prices[i] > prices[bestSellPoint] - prices[bestBuyPoint])
+                    {
+                        bestBuyPoint = i;
+                        bestSellPoint = j;
+                    }
+                }
+            }
+
+            return prices[bestSellPoint] - prices[bestBuyPoint];
+        }
+    }
+}
