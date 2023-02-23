f56050f4c1a076f00e377705f8455b78358cbbe0
Tomasz Jagiełka <thomas.jagielka@gmail.com>
2023-02-23 12:23:47 +0100

    1480. Running Sum of 1d Array

---
+++ C-Sharp/C-Sharp/1480RunningSumOf1dArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class _480RunningSumOf1dArray
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            int counter = 0;
+            int[] runningNums = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j <= counter; j++)
+                    sum += nums[j];
+
+                runningNums[i] = sum;
+                counter++;
+            }
+
+            return runningNums;
+        }
+    }
+}
