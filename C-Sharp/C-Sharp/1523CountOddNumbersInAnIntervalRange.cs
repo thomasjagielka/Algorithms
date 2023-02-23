88577ef939772d82730057b5994379d49cd5e0f7
Tomasz Jagiełka <thomas.jagielka@gmail.com>
2023-02-23 22:32:30 +0100

    1523. Count Odd Numbers in an Interval Range

---
+++ C-Sharp/C-Sharp/1523CountOddNumbersInAnIntervalRange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class _1523CountOddNumbersInAnIntervalRange
+    {
+        public static int CountOdds(int low, int high)
+        {
+            if ((low & 1) == 0) low++;
+            return low > high ? 0 : (high - low) / 2 + 1;
+        }
+    }
+}
